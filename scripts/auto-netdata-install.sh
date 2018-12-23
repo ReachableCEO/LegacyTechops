a8dbcfae03959107ebd9f745893f92e5d69a9cd9
Charles Wyble <charles@turnsys.com>
2018-12-23 12:36:02 -0600

    capturing all the nodes

---
+++ scripts/auto-netdata-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -Ss 'https://raw.githubusercontent.com/netdata/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh --dont-wait -i netdata-all
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
+
+
