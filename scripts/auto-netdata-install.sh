b23f857c6c87c288d8db4c1f4b98bbdc0e3ac724
Charles Wyble <charles@turnsys.com>
2019-02-24 10:21:11 -0600

    updated inventory

--- scripts/auto-netdata-install.sh
+++ scripts/auto-netdata-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -Ss 'https://raw.githubusercontent.com/netdata/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh --dont-wait -i netdata-all
+curl -Ss 'https://raw.githubusercontent.com/netdata/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh 
+bash /tmp/kickstart.sh --dont-wait -i netdata-all
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
 
 
